5ed2d4ac032d86ae59a9e2d1362218ac3aabbcb9
Pumaru_05 <74053113+Pumaru05@users.noreply.github.com>
2023-05-19 16:43:50 +0200

    Implementation of the Subrace Drake: Fire
    Implemented the Subrace Volcanic Drake, who, is a subrace descends from the race of Elemental Fire Dragons.

--- lizards/drakes.js
+++ lizards/drakes.js
@@ -115,4 +115,14 @@ class fireSubrace1 {
     }
 }
 
+class fireSubrace2 {
+    constructor (name, race, element, habitat, location) {
+        name = "Volcanic Drake";
+        race = FDrake.nameRace;
+        element = FDrake.element;
+        habitat = "Volcanic biomes and/or ashy and arid terrains"
+        location = "Lives in the slopes of the volcano, especially, where volcanic rocks and ash from ancient eruptions around"
+    }
+}
+
 export { drake };
