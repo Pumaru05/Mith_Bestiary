f1483ccc0ed9da0bbaab966faeb44f397ec936ef
Pumaru_05 <74053113+Pumaru05@users.noreply.github.com>
2023-07-06 19:58:27 +0200

    Basic definition of the prototype animal
    Defined with a basic method a prototype animal class.

--- script.js
+++ script.js
@@ -1,7 +1,58 @@
-import {lizard} from "/lizards/drakes/drakes.js";
+// Importing Classes
+import {bird} from "birds/bird.js";
+import {lizard} from "lizards/lizard.js";
+import {mamifer} from "mamifers/mamifer.js";
+
+// Importing JSONs
+    // JSON Habits
+    var sleep_habits   = "JSONs/habits/sleepHabits/sleepingHabits.json";
+    var walking_types  = "JSONs/habits/walkingTypes/walkingTypes.json";
+    var eating_types   = "JSONs/habits/eatingTypes/eatingTypes.json";
+    var drinking_types = "JSONs/habits/drinkingTypes/drinkingTypes.json";
+    var running_types  = "JSONs/habits/runningTypes/runningTypes.json";
+    var attacks_types  = "JSONs/habits/attackingTypes/attackingTypes.json";
+
+    // JSON Characteristics
+    var weapons_types      = "JSONs/characteristics/weaponTypes/weaponTypes.json";
+    var defenses_types     = "JSONs/characteristics/defensesTypes/defensesTypes.json";
+    var addaptations_types = "JSONs/characteristics/addaptationsTypes/addaptationsTypes.json";
+
+//
 
 class animal {
-    constructor (type) {
-        type = lizard;
+    constructor (types, habits, characteristics) {
+        types           = animal_types;
+        habits          = animal_habits;
+        characteristics = animal_characteristics;
     }
 }
+
+class animal_types {
+    constructor (type_1, type_2, type_3) {
+        type_1 = bird;
+        type_2 = lizard;
+        type_3 = mamifer;
+    }
+}
+
+class animal_habits {
+    constructor (sleep, walk, eat, drink, run, attacks) {
+        sleep = sleep_habits;
+        walk = walking_types;
+        eat = eating_types;
+        drink = drinking_types;
+        run = running_types;
+        attacks = attacks_types;
+    }
+}
+
+class animal_characteristics {
+    constructor (weapons, defenses, addaptations, ) {
+        weapons      = weapons_types;
+        defenses     = defenses_types;
+        addaptations = addaptations_types;
+    }
+}
+
+export { sleep_habits, walking_types, eating_types, drinking_types, running_types, attacks_types,
+         weapons_types, defenses_types, addaptations_types };
