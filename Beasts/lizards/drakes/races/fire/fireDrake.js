4a6be41c1662602a062e5bbff742ce13929b6449
Pumaru_05 <74053113+Pumaru05@users.noreply.github.com>
2024-06-11 15:16:16 +0200

    Correction patch
    Little correction of the code.

--- Beasts/lizards/drakes/races/fire/fireDrake.js
+++ Beasts/lizards/drakes/races/fire/fireDrake.js
@@ -1,12 +1,11 @@
 import { FSubRace } from "../../subraces/fireSubraces/fireSubrace.js";
 
 class FDrake {
-    constructor (nameRace, element, habitat, location, subrace) {
+    constructor (nameRace, element, habitat, location) {
         nameRace = "Fire Drake";
         element = "Fire";
         habitat = "Fire Biomes";
         location = "Hot and/or volcanic places and territories";
-        subrace = FSubRace;
     }
 }
 
--- Beasts/lizards/drakes/races/ice/iceDrake.js
+++ Beasts/lizards/drakes/races/ice/iceDrake.js
@@ -1,12 +1,11 @@
 import { ISubRace } from "../../subraces/iceSubraces/iceSubrace.js";
 
 class IDrake {
-    constructor (nameRace, element, habitat, location, subrace) {
+    constructor (nameRace, element, habitat, location) {
         nameRace = "Ice Drake";
         element = "Ice";
         habitat = "Ice Biomes";
         location = "High and snowy mountains and/or deep and icy caves";
-        subrace = ISubRace;
     }
 }
 
