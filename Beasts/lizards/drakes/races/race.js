cc637460f74d87fd0f335c771356a6af75baf270
Pumaru_05 <74053113+Pumaru05@users.noreply.github.com>
2024-06-11 15:08:52 +0200

    Correction of the Drakes
    Erase of some races of drakes for a better developing (with a chances of including the races erased in the future)

--- Beasts/lizards/drakes/races/race.js
+++ Beasts/lizards/drakes/races/race.js
@@ -1,16 +1,10 @@
 import { FDrake } from "fire/fireDrake.js";
 import { IDrake } from "ice/iceDrake.js";
-import { EDrake } from "electric/electricDrake.js";
-import { TDrake } from "terran/terranDrake.js";
-import { DaDrake } from "dark/darkDrake.js";
 
 class race {
-    constructor (idfDrake, idiDrake, ideDrake, idtDrake, iddaDrake) {
+    constructor (idfDrake, idiDrake) {
         idfDrake = FDrake;
         idiDrake = IDrake;
-        ideDrake = EDrake;
-        idtDrake = TDrake;
-        iddaDrake = DaDrake;
     }
 }
 
--- Beasts/lizards/drakes/subraces/subrace.js
+++ Beasts/lizards/drakes/subraces/subrace.js
@@ -1,17 +1,11 @@
 import { FSubRace } from "/fireSubraces/fireSubrace.js";
 import { ISubRace } from "/iceSubraces/iceSubrace.js";
-import { ESubRace } from "/electricSubraces/electricSubrace.js";
-import { TSubRace } from "/terranSubraces/terranSubrace.js";
-import { DaSubRace } from "/darkSubraces/darkSubrace.js";
 // import { DragonWalker } from "";
 
 class drake_subraces {
-    constructor (idFDrakeSRace, idIDrakeSRace, idEDrakeSRace, idTDrakeSRace, idDaDrakeSRace, idDragonWalker) {
+    constructor (idFDrakeSRace, idIDrakeSRace) {
         idFDrakeSRace = FSubRace;
         idIDrakeSRace = ISubRace;
-        idEDrakeSRace = ESubRace;
-        idTDrakeSRace = TSubRace;
-        idDaDrakeSRace = DaSubRace;
         // idDragonWalker = DragonWalker;
     }
 }
